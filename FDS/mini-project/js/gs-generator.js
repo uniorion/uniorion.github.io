2f6d31e301832f478bf7412f2e241459e8963af2
LEE JIN KI <uniorion@gmail.com>
2016-10-18 22:45:38 +0900

    미니프로젝트 진행

--- FDS/mini-project/js/gs-generator.js
+++ FDS/mini-project/js/gs-generator.js
@@ -1,28 +1,27 @@
+const  GRID_COL_COLOR 	= 'hsla(163, 64%, 49%, 0.2)'
+ 		 , GRID_LINE_COLOR	= '#7400ff';
 
 var 	unit = 'px';
 
 // 요소찾아 변수에 참조
-var 	el_gs_type_stt 		= document.getElementById('rdo_gs_type_stt')
-		, el_gs_type_fld 		= document.getElementById('rdo_gs_type_fld')
-		, el_total_width 		= document.getElementById('txt_total_width')
-		, el_columns 				= document.getElementById('txt_columns')
-		, el_margin_width 	= document.getElementById('txt_margin_width')
-		, el_gutter_width 	= document.getElementById('txt_gutter_width')
-		, el_txta_css				= document.getElementById('txta_css')
-		, el_btn_calculate 	= document.getElementsByClassName('btn-calculate')[0]
-		, el_btn_download 	= document.getElementsByClassName('btn-download')[0]
-		, el_content_area 	= document.getElementById('txt_content_area')
-		, el_column_width 	= document.getElementById('txt_column_width')
-		, el_fixed_margin 	= document.getElementById('txt_fixed_margin');
-
-
+var 	el_gs_type_stt 		= document.querySelector('#rdo_gs_type_stt')
+		, el_gs_type_fld 		= document.querySelector('#rdo_gs_type_fld')
+		, el_total_width 		= document.querySelector('#txt_total_width')
+		, el_columns 				= document.querySelector('#txt_columns')
+		, el_margin_width 	= document.querySelector('#txt_margin_width')
+		, el_gutter_width 	= document.querySelector('#txt_gutter_width')
+		, el_txta_css				= document.querySelector('#txta_css')
+		, el_btn_calculate 	= document.querySelector('.btn-calculate')
+		, el_btn_download 	= document.querySelector('.btn-download')
+		, el_content_area 	= document.querySelector('#txt_content_area')
+		, el_column_width 	= document.querySelector('#txt_column_width')
+		, el_fixed_margin 	= document.querySelector('#txt_fixed_margin');
 
 var  	gs_type_val 	= getRadioCheckedValue('rdo_gs_type')
 		, total_width 	= 0
 		,	columns 			= 0
 		,	margin_width 	= window.parseInt(el_margin_width.value, 10)
 		,	gutter_width 	= null;
-
 
 var 	margin_width_fix 		= 0
     , column_width 				= 0
@@ -31,13 +30,17 @@ var 	margin_width_fix 		= 0
     , total_column_width	= 0
     , mod_column_width 		= 0;
 
-
 function setInputValue() {
 	total_width 		= window.parseInt(el_total_width.value, 10);
 	columns 				= window.parseInt(el_columns.value, 10);
 	margin_width 		= window.parseInt(el_margin_width.value, 10);
 	gutter_width 		= window.parseInt(el_gutter_width.value, 10);	
 	margin_width_fix = margin_width;
+
+	//---------------- 입력값 검증
+	if ( columns === 1 ) {
+		gutter_width = 0;			// 컬럼이 하나이면 거터값 무시하고 계산
+	}
 }
 	
 // 그리드 요소 값 계산
@@ -62,8 +65,8 @@ function calcGrid() {
 	else if ( isEven( mod_column_width ) === false ) 	// 총컬럼 넓이를 컬럼수로 나눈 값이 홀수이면
 	{
 		console.log("calcGrid() : odd");
-		if ( isEven( columns ) === true ) {	// 나머지가 홀수일 때, 컬럼수가 짝수이면 정수 계산이 안됨.  960-6-0-13, 960-7-0-14
-			console.log("정수 계산 안됨.");					// ************* 경고 디스플레이 처리 필요
+		if ( isEven( columns ) === true ) {	// 나머지가 홀수일 때, 컬럼수가 짝수이면 정수 계산이 안됨.
+			console.log("정수로 계산 안됨.");					// ************* 경고 디스플레이 처리 필요
 			return;
 		}
 		mod_column_width = mod_column_width + 1;
@@ -77,11 +80,8 @@ function displayResult() {
 	el_content_area.value = total_width;
 	el_column_width.value = column_width;
 	el_fixed_margin.value	= margin_width_fix;
-}
-
-	console.log("column_width : ", column_width);
-	console.log("margin_width_fix : ", margin_width_fix);
-	console.log("mod_column_width : ", mod_column_width);
+	el_txta_css.innerText = doc_CSS;
+} 
 
 function isType(data) {
     return Object.prototype.toString.call(data).toLowerCase().slice(8, -1);
@@ -130,12 +130,12 @@ function getRadioCheckedValue(rdo_name) {
 /*
 	이벤트 연결
 */
-// Calculate
-el_btn_calculate.onclick = function()
-{
+// Calculate 버튼
+el_btn_calculate.onclick = function() {
 	setInputValue();
 	calcGrid();
 	displayResult();
+	setCSS();
 };
 // 라디오 버튼
 el_gs_type_stt.onclick = function() {
--- FDS/mini-project/js/gs-make-css.js
+++ FDS/mini-project/js/gs-make-css.js
@@ -1,12 +1,11 @@
-var	docCSS = null;	// 출력될 css 문서 전체
- 
-doc_CSS = "@charset 'utf-8';\n"
+var	doc_CSS = null;	// 출력될 css 문서
 
 // css 문서내용 만들기
 function setCSS() {
 	attachComment();
 	attachContainer();
-	el_txta_css.innerText = doc_CSS;
+	attachGuide();
+	el_txta_css.textContent = doc_CSS;
 }
 
 
@@ -16,16 +15,15 @@ function setCSS() {
 
 // 상단 주석
 function attachComment() {
-	doc_CSS =	doc_CSS
-					+ "\n/* ======================================="
+	doc_CSS =	"\n/* ======================================="
 					+ "\n 	Grid System CSS v1.0 - Static"
 					+ "\n # width : " + total_width + unit
 					+ "\n # columns : " + columns
-					+ "\n # margin : " + margin_fix + unit
-					+ "\n # gutter : " + gutter + unit
+					+ "\n # margin : " + margin_width_fix + unit
+					+ "\n # gutter : " + gutter_width + unit
 					+ "\n========================================== */";
 }
-// container 스타일
+// container 모듈
 function attachContainer() {
 	doc_CSS =	doc_CSS 
 					+ "\n\n.container {"
@@ -54,7 +52,43 @@ function attachContainer() {
 // show-grid
 // show-baseline
 // show-columns
+function attachGuide() {
+		var column_gradient = "";
 
+		column_gradient = "transparent " + margin_width_fix + unit
+									 	+ ", " + GRID_COL_COLOR + " " + margin_width_fix + unit
+									 	+ ", " + GRID_COL_COLOR + " " + (margin_width_fix + column_width) + unit;
+
+		for (var i = 0; columns - 1 > i; ++i) {
+			column_gradient = column_gradient 
+											+ ", transparent " + (margin_width_fix + (column_width * i) + (gutter_width * i)) + unit;
+			 								+ ", " + GRID_COL_COLOR + " " + (margin_width_fix + (column_width * i) + (gutter_width * i)) + unit
+			 								+ ", transparent " + margin_width_fix + (column_width * i) + (gutter_width * i) + unit;
+
+		}
+
+		column_gradient = column_gradient
+										+ ", transparent " + (total_width - margin_width_fix) + unit;
+
+
+
+		// show-baseline
+		doc_CSS =	doc_CSS 
+						+ "\n.container.show-baseline::before {"
+						+ "\nbackground-image: linear-gradient(180deg, transparent 9px, " + GRID_LINE_COLOR + " 9px);"
+						+ "\nbackground-size: 1px 10px; }"
+		doc_CSS =	doc_CSS
+						+ "\n.container.show-columns::before {"
+						+ "\nbackground-image: linear-gradient(90deg, " + column_gradient + ");"
+						+ "\nbackground-size: " + total_width + "px 1px; } "
+
+}
+
+
+// + "\n.container.show-grid::before {
+//   background-image: linear-gradient(180deg, transparent 9px, #7400ff 9px), linear-gradient(90deg, hsla(237, 100%, 66%, 0.3), hsla(239, 100%, 79%, 0.3));
+//   background-size: 1px 10px, 120px 1px;
+// }
 
 /*****************
 		columns
