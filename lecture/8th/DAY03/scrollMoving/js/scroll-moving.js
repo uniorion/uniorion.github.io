ff61da280349f74443021c093636694289176124
uniorion <uniorion@naver.com>
2016-11-10 22:32:54 +0900

    rename : practice > lecture

--- lecture/8th/DAY03/scrollMoving/js/scroll-moving.js
+++ lecture/8th/DAY03/scrollMoving/js/scroll-moving.js
@@ -5,16 +5,14 @@ jQuery(function($) {
   var $scroll_menu_ul = $('.scroll-menu ul');
 
   $('.scroll-menu a').on('click', function(evt) {
-    console.dir(evt);
     evt.preventDefault();
-    evt.stopPropagation();
     var scroll_menu_ul_height = window.parseInt( $scroll_menu_ul.css('height'), 10 );
     var target_id             = this.getAttribute('href');
     var $target               = $(target_id);
 
-    $doc.animate({
+    $doc.stop().animate({
       'scrollTop': $target.offset().top - scroll_menu_ul_height
-    }, 400, 'easeInOutCirc');
+    }, 200, 'easeInOutCirc');
   });
 
 });
