7b168be5f1daa178e0ed11921d5150b49c10b07a
uniorion <uniorion@naver.com>
2016-11-19 00:26:23 +0900

    161118 수업 진행 파일

---
+++ lecture/9th/DAY05/dist/assets/app.js
@@ -0,0 +1,2 @@
+!function(e){function o(r){if(n[r])return n[r].exports;var t=n[r]={exports:{},id:r,loaded:!1};return e[r].call(t.exports,t,t.exports,o),t.loaded=!0,t.exports}var n={};return o.m=e,o.c=n,o.p="/assets",o(0)}([function(e,o,n){/*! app.js © yamoo9.net, 2016 */
+"use strict";n(1),n(5);var r=n(6);console.log(r.version.full),console.log(r.element.fn.jquery)},function(e,o){},,,,function(e,o){e.exports=jQuery},function(e,o){e.exports=angular}]);
---
+++ lecture/9th/DAY05/src/app/filters/capitalizeFilter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var angular = require('angular');
+
+angular.module('FDS_Utils_Module').filter('capitalize', function() {
+  return function(input, seperator) {
+    seperator = seperator || ' ';
+    return input.split(seperator).map(function(s) {
+      return s.replace(/./, function($1){ return $1.toUpperCase(); });
+    }).join(' ');
+  };
+});
+
+
+angular.module('FDS_Utils_Module').filter('camelCase', function() {
+  return function(input, seperator) {
+    seperator = seperator || ' ';
+    return input.split(seperator).map(function(s, i) {
+      if ( i === 0 ) { return s; }
+      return s.replace(/./, function($1){ return $1.toUpperCase(); });
+    }).join('');
+  };
+});
